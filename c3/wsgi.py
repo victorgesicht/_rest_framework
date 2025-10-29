50d672d90aabe1f732484307cc234900059c5e76
victorgesicht <victorwachira100@gmail.com>
2025-10-29 15:46:57 +0300

    uuurgh gunicorn

--- c3/wsgi.py
+++ c3/wsgi.py
@@ -1,6 +1,7 @@
 import os
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c3.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 application = get_wsgi_application()
-
