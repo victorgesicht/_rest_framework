d9086bb3d020e423ae528d7629db058762800101
victorgesicht <victorwachira100@gmail.com>
2025-10-29 15:40:41 +0300

    WSGI setup

--- c3/wsgi.py
+++ c3/wsgi.py
@@ -1,7 +1,6 @@
 import os
-from configurations.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c3.settings')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')  # or your config class
 
 application = get_wsgi_application()
+
