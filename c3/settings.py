7b007a547ba6a747f80ea242b353fb50042ebc5b
victorgesicht <victorwachira100@gmail.com>
2025-11-02 10:33:03 +0300

    _

--- c3/settings.py
+++ c3/settings.py
@@ -187,7 +187,9 @@ class Prod(Dev):
     DEBUG = False
     SECRET_KEY=values.SecretValue()
     ALLOWED_HOSTS= values.ListValue(['9t6-production.up.railway.app'])
-
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.config(
+            conn_max_age=600,       # Keep database connections open for performance
+            ssl_require=True        # Enforce SSL (Railway Postgres uses SSL)
+        )
     }
